13384a369b4b55c80c6d8db1fd6dd340f922f8ce
George Howarth <george@pyesmeadow.com>
2023-10-28 16:24:44 +0100

    Add remaining files

---
+++ src/com/pyesmeadow/george/instruction/SetLessThanImmediateInstruction.java
@@ -0,0 +1,14 @@
+package com.pyesmeadow.george.instruction;
+
+import com.pyesmeadow.george.Hart;
+
+public class SetLessThanImmediateInstruction extends TypeIInstruction {
+    public SetLessThanImmediateInstruction(int instruction) {
+        super(instruction);
+    }
+
+    @Override
+    public void execute(Hart hart) {
+
+    }
+}
---
+++ src/com/pyesmeadow/george/instruction/TypeIInstruction.java
@@ -0,0 +1,18 @@
+package com.pyesmeadow.george.instruction;
+
+public abstract class TypeIInstruction implements IInstruction {
+
+    final int opcode;
+    final int rd;
+    final int funct3;
+    final int rs1;
+    final int imm;
+
+    public TypeIInstruction(int instruction) {
+        opcode = instruction &0b1111111;
+        rd = (instruction >> 7) & 0b11111;
+        funct3 = (instruction >> 12) & 0b111;
+        rs1 = (instruction >> 15) & 0b11111;
+        imm = (instruction >> 20);
+    }
+}
